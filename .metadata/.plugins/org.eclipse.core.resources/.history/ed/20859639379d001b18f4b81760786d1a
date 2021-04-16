d477b461fe6f1ef786cc1e73e63902e5773106e7
Ditmar <ditmarcastro@gmail.com>
2021-04-16 10:42:34 -0400

    examples progra 2

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ed/20859639379d001b18f4b81760786d1a
@@ -0,0 +1,12 @@
+package example1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point3d point = new Point3d(2.3, 3.4, 5.6);
+		System.out.println(point.getX() + " , " + point.getY() + ", " + point.getZ());
+		
+	}
+
+}
---
+++ example1/src/example1/Persona.java
@@ -0,0 +1,29 @@
+package example1;
+
+final public class Persona {
+	private String name;
+	private String lastname;
+	private String age;
+	public Persona () {
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+}
